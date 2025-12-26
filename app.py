78f341783a3f067a6bdf728806f42a3f38edab4e
kabirera03-ship-it <kabirera03@gmail.com>
2025-12-26 14:22:41 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
+import time
+import sys
+
 def print_lyrics():
  lyrics = [
- "Haathon ko sambhale mere haathon mein",
- "Kaise haathon ko sambhale mere haathon mein..",
- "Jab tak neend na aaye, inn lakeeron mein..",
- "Baatein ho........",
- "Haaye"
+ "Sambhaal ke rakha wo phool mera tu",
+ "Meri shayari mein zaroor raha tu",
+ "Jo aankhon mein pyaari si duniya basaayi",
+ "Wo duniya bhi tha tu, wo lamha bhi tha tu",
+ "Haan, lagte hain mujhko ye kisse sataane",
+ "Deta na dil mera tujhko bhulaane",
+ "Adhoore se vaade, adhoori si raatein",
+ "Ab hisse mein daakhil mere bas wo yaadein"
  ]
 
-  delays = [1.0, 0.1, 1.12, 0.9, 0.8]
+  delays = [0.7, 0.7, 0.4, 0.5, 0.5, 0.7, 0.7, 1.0]
 
-  print("Arz kya hai?.......:\n")
- time.sleep(1.4)
+  print("Finding Her: \n")
+ time.sleep(1.2)
 
   for i, line in enumerate(lyrics):
  for char in line:
  sys.stdout.write(char)
  sys.stdout.flush()
- time.sleep(0.1)
- print()
+ time.sleep(0.1) 
+  print()
  time.sleep(delays[i])
 
 print_lyrics()
